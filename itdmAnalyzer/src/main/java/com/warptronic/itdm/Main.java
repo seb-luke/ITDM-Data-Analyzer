75d370324e3fa5114d08e49581ba88da71f0f20d
Sebastian Luca <seb.luke@gmail.com>
2017-06-05 20:45:46 +0300

    Added two more cases with big data

--- itdmAnalyzer/src/main/java/com/warptronic/itdm/Main.java
+++ itdmAnalyzer/src/main/java/com/warptronic/itdm/Main.java
@@ -7,12 +7,18 @@ import com.warptronic.itdm.config.CredentialsException;
 public class Main {
 
 	public static void main(String[] args) throws CredentialsException {
-		
+
 		Case hibernateCase = new Case("https://hibernate.atlassian.net/", "", "", "", "", "HHH");
 		new Analyzer(hibernateCase, "Hibernate_HHH.txt").analyzeAndWrite();
 		
 		Case springCase = new Case("https://jira.spring.io", "", "", "", "", "SPR");
 		new Analyzer(springCase, "Spring_SPR.txt").analyzeAndWrite();
+		
+		Case jBossCase = new Case("https://issues.jboss.org", "", "", "", "", "JBEAP");
+		new Analyzer(jBossCase, "JBoss_JBEAP.txt").analyzeAndWrite();
+		
+		Case jiraCase = new Case("https://jira.atlassian.com", "", "", "", "", "JRA");
+		new Analyzer(jiraCase, "AtlassianJira_JRA.txt").analyzeAndWrite();
 		
 		
  		System.out.println("\n\n\t\tDone");
